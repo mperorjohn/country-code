9fa8059a82a569b24494bf329c8fca42a55a631e
John <Mperorjohnoyekola@gmail.com>
2023-02-13 12:02:44 +0100

    New look

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <h1>Search your country code</h1>
       <div className="queryCountry">
         <form action="">
-          <input type='text' className='queryParameter' placeholder='Search country code'/><br/>
+          <input type='text' className='queryParameter' placeholder='Enter country'/><br/>
           <br/>
           <button type='submit' className='search-btn'>Search</button>
         </form>

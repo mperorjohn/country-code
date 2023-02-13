9b76f86b52f702ea8acdffb06a8fbd629c8ceb18
John <Mperorjohnoyekola@gmail.com>
2023-02-13 07:36:59 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="queryCountry">
+        <form action="">
+          <input type='text' className='queryParameter' placeholder='Search country code'/><br/>
+          <br/>
+          <button type='submit' className='search-btn'>Search</button>
+        </form>
+      </div>
+      <div className="showQuery">
+
+      </div>
+
     </div>
   );
 }
